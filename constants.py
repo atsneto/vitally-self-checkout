62de772886a0f5018856152305d4f97d2660f237
atsneto <toscanosorrentinonetoa@gmail.com>
2025-06-03 10:28:16 -0300

    Tela de classificação e temperatura, config do sensor

---
+++ constants.py
@@ -0,0 +1,17 @@
+# Criar um arquivo separado para constantes (constants.py)
+import flet as ft
+
+
+class Colors:
+    PRIMARY = ft.colors.BLUE_800
+    SUCCESS = ft.colors.GREEN_400
+    WARNING = ft.colors.YELLOW_400
+    DANGER = ft.colors.RED_400
+    INFO = ft.colors.BLUE_400
+    WHITE = ft.colors.WHITE
+
+class Assets:
+    LOGO = "images/vitally_logo.png"
+    BIOMETRIA_GIF = "images/biometriafacial.gif"
+    SCAN_ACTIVE = "images/scan_active.gif"
+    SUCCESS_CHECK = "images/success_checkmark.gif"
